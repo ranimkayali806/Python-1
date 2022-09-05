e75617f3343a75e7ac679c7645dc1d2eff54c81a
Ranim Kayali <eng.ranimkayali@gmail.com>
2022-09-05 14:00:10 +0200

    first commit

---
+++ main.py
@@ -0,0 +1,60 @@
+#print("Hej")
+#print("Då")
+#print("2022-12-24")
+#print(2022-12-24)
+# Age = 50
+#print("Vad heter du?")
+#namnet = input("")
+#print("Hejsan", namnet)
+ #yearsToAdd = input("Hur många år vill du lägga till")
+#Age = Age +int(yearsToAdd)
+#print(Age)
+#namn = "Ranim"
+#age = 30 
+#förenamn = input("skriv in ditt förenamn")
+#efternamn = input("skriv in ditt efternamn")
+#print("du heter" ,":", efternamn, ",", förenamn)
+#print("jag heter" ,namn, "och", "jag är" ,age, "år" )
+#antalDagar = 25
+#print("så har många kursen:" , antalDagar) 
+#tal1 = input("Tal1:")
+#tal2 = int(input("Tal2:"))
+#print(tal1 + tal2)
+#tal1 = int(input("Mata in tal 1"))
+#tal2 = int(input("Mata in tal2"))
+#print(tal1**tal2)
+#print(tal1+tal2)
+#n = int(input("skriv in ett tal:"))
+#res = n > 100
+#print(True)
+#res = n < 100
+#print(False)
+# age = int(input("Hur gammal du?"))
+# location = input("Var är du?")
+# promilleHalt = float(input("Din promilleHalt"))
+
+# if age >= 18 and location == "k" and promilleHalt < 1.0:
+#     print("Japp du får köpa")
+#     elif age >= 20 and location == "s" and promilleHalt < 1.0:
+#     print("Japp du får köpa")
+#     else:
+#         print("Nope ingen öl")
+#age = int(input("Hur gammal du?"))
+#if age < 18:
+ #print("Du är omyndig")
+    #yearsToadd18 = 18 - age
+    #print("Du får vänta" , yearsToadd18 ,"tills du är myndig")
+#else:
+     #print("Du är myndig")
+     #print("klart")
+
+     #F1
+     Be användaren att mata in ett tal. 
+
+Kontrollera om talet är större än 10. Meddela
+
+användaren om talet är större än 10- Talet är större än 10. Om det är mindre än 10
+
+meddela användaren
+
+– Talet är mindre än 10
